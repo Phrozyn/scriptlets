f31e858306a8d5f031ea4761abc0ba54bfc4cd3a
A Smith <Phrozyn@users.noreply.github.com>
2018-05-23 10:49:57 -0500

    Create downtime.sh

---
+++ downtime.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# To use this file simply call ./downtime.sh hostlist period note
+# where hostlist is a file with hosts one on each line, period is either
+# a future hour, or in absolute values like "2 hours", and note is why you
+# are downtiming the host.
+
+hostfile=$1
+period=$2
+note=$3
+
+echo $hostfile name
+echo $# arguments
+
+cat "$hostfile" | while read line; do
+  sudo /data/bin/nagiosctl downtime $line "$period" "$note"
+done
