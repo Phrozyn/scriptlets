5fca5e9cacc2364aa0adc83cff90771ce1a0a3d3
Phrozyn <alicia.smith@quantum.security>
2020-10-06 16:56:07 -0500

    removing test comment

--- downtime.sh
+++ downtime.sh
@@ -4,7 +4,6 @@
 # where hostlist is a file with hosts one on each line, period is either
 # a future hour, or in absolute values like "2 hours", and note is why you
 # are downtiming the host.
-# testline
 
 hostfile=$1
 period=$2
